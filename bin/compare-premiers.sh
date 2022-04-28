19668e4fb02633d125ed23f0ef54de8df6f91d0c
Tony Bowden <tony@tmtm.com>
2022-04-28 20:22:13 +0300

    move Premiers comparisong to sources/

--- bin/compare-premiers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-bundle exec ruby bin/scraper/premiers-wikipedia.rb > data/premiers-wikipedia.csv
-# WD list fetched separately TODO: move together
-bundle exec ruby bin/diff-premiers.rb > data/diff-premiers.csv
